bb73631092e1915aac613b0c5cac3f65334bad2b
nurzatEjat <nurzatejat@live.com>
2019-08-08 19:57:12 -0400

    Test

--- amazonProject/src/test/java/amazonPage/ProductListPage.java
+++ amazonProject/src/test/java/amazonPage/ProductListPage.java
@@ -9,6 +9,8 @@ import utilityLibrary.SetUpPage;
 
 public class ProductListPage extends SetUpPage{
 	List<WebElement> listOfProductNames = driver.findElements(By.xpath("//*[@data-index]//span[contains(@class,'text-normal') and text()]")); 
+	WebElement dropdown = driver.findElement(By.xpath("//select[@class='a-native-dropdown']"));
+	
 	
 	public void verifyLandOnpage() {
 		driver.findElement(By.id("s-refinements"));
@@ -22,4 +24,11 @@ public class ProductListPage extends SetUpPage{
 		}
 		return names;
 	}
+	
+	public void sortByIndex(int index) {
+		lab.selectDropdownByIndex(dropdown, index);
+	}
+	
+	
+	
 }
--- amazonProject/src/test/java/amazonProject/HeaderTest.java
+++ amazonProject/src/test/java/amazonProject/HeaderTest.java
@@ -8,50 +8,32 @@ import org.testng.annotations.Test;
 import amazonPage.HeaderPage;
 import utilityLibrary.SetUpPage;
 
-public class HeaderTest extends SetUpPage{
+public class HeaderTest extends SetUpPage {
 
-
-
-	
 	@Test(enabled = false)
 	public void AP_1001() {
 
 		header.verifyHomePageTitle();
 		header.clickTodayDealLink();
 		TDP.verifyTodayDealPage();
-		
+
 		TDP.selectBookFromLeftNavigationBar();
-		
+
 		lab.wait(2);
-		String bookLink = driver.findElement(
-				By.xpath("//*[@class='a-link-normal summary']/span")).getText().trim();
-		
+		String bookLink = driver.findElement(By.xpath("//*[@class='a-link-normal summary']/span")).getText().trim();
+
 		Assert.assertTrue(bookLink.equals("Books"), "Facet NOT working as expect, Please check!");
-		
 
 	}
 
-
-
-
-
-
-
-
-
-
-
-
-	
 	@Test(enabled = false)
 	public void AP_1002() {
-		String expectPageTitle = 
-				"Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more";
+		String expectPageTitle = "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more";
 		String title = driver.getTitle();
-		//Overloading -> it will allowed us to select any 
-		//      assertEquals and passing differnt parameter without change metho name
+		// Overloading -> it will allowed us to select any
+		// assertEquals and passing differnt parameter without change metho name
 		Assert.assertEquals(title, expectPageTitle, "User NOT land on HomePage!");
-		
+
 		WebElement tryPrime = driver.findElement(By.xpath("//a[contains(text(),'Try Prime')]"));
 		// move our mouse
 		lab.moveMouseByElement(tryPrime);
@@ -61,47 +43,44 @@ public class HeaderTest extends SetUpPage{
 		driver.findElement(By.xpath("//*[@id='OfferSlot']//*[@class='prime-cta-signup-button-inner']")).click();
 		boolean isSignInDisplay = false;
 		try {
-		 isSignInDisplay = driver.findElement(By.id("signInSubmit")).isDisplayed();
-		}catch(Exception e) {
-			
+			isSignInDisplay = driver.findElement(By.id("signInSubmit")).isDisplayed();
+		} catch (Exception e) {
+
 		}
 		Assert.assertTrue(isSignInDisplay, "User NOT land on Sign In page!");
 	}
-	
 
-	@Test
+	@Test(enabled = true)
 	public void AP_1004() {
-		
+
 		header.verifyHomePageTitle();
 		header.clickTodayDealLink();
 		TDP.verifyTodayDealPage();
 		TDP.selectBookFromLeftNavigationBar();
 		TDP.sortPrice(2);
 		lab.wait(5);
-		
+
 	}
-	
+
 	/**
-	 * Verify search functionality 
+	 * Verify search functionality
 	 */
+	
 	@Test
 	public void AP_1005() {
 		header.verifyHomePageTitle();
 		header.fillSearchBox("camera");
 		plp.verifyLandOnpage();
-		for(String temp :plp.getProductName()) {
-			if(temp.contains("camera")) {
+		for (String temp : plp.getProductName()) {
+			if (temp.contains("camera")) {
 				System.out.println(temp);
-			}else {
-				Assert.fail("Product name doesn't contains: camera "+temp);
+			} else {
+				Assert.fail("Product name doesn't contains: camera " + temp);
 			}
 		}
-		
-		
-		
-		
+
+		plp.sortByIndex(2);
+
 	}
 
-	
-	
 }
